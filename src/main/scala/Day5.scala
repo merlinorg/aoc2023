07feda8d14b5278c2f2cd41e65df06aeae2f5e3a
merlin <merlin@merlin.org>
2023-12-05 12:28:26 -0500

    not so brutish

--- src/main/scala/Day5.scala
+++ src/main/scala/Day5.scala
@@ -1,68 +1,79 @@
 package org.merlin.aoc2023
 
+import scalaz.*
 import scalaz.Scalaz.*
 
 import scala.annotation.tailrec
-import scala.collection.immutable.SortedMap
 
 object Day5 extends AoC:
-  // start - 1 -> range
-  private type Ranges = SortedMap[Long, Range]
-  // from -> to, ranges
-  private type Maps   = Map[String, (String, Ranges)]
+  private final case class Mapping(dst: Long, src: Long, len: Long):
+    def maps(value: Long): Boolean     = value >= src && value < src + len
+    def mapsTo(value: Long): Boolean   = value >= dst && value < dst + len
+    def map(value: Long): Option[Long] = maps(value).option(value + dst - src)
+    def take(n: Long): Mapping         = copy(len = n)
+    def drop(n: Long): Mapping         = copy(dst = dst + n, src = src + n, len - n)
+  end Mapping
 
-  private final case class Range(dst: Long, src: Long, len: Long) {
-    def contains(value: Long): Boolean = value >= src && value < src + len
-    def apply(value: Long): Long       = value + dst - src
-  }
-
-  @tailrec private def parseRanges(
-    l: List[String],
-    ranges: Ranges = SortedMap.empty
-  ): (Ranges, List[String]) = l match
+  @tailrec private def parseMappings(
+    input: List[String],
+    mappings: List[Mapping] = Nil
+  ): (List[Mapping], List[String]) = input match
     case s"$dst $src $len" :: rest =>
-      val range = Range(dst.toLong, src.toLong, len.toLong)
-      parseRanges(
+      val mapping = Mapping(dst.toLong, src.toLong, len.toLong)
+      parseMappings(
         rest,
-        ranges.updated(range.src - 1, range),
+        mapping :: mappings,
       )
-    case "" :: rest                => (ranges, rest)
-    case _                         => (ranges, Nil)
+    case "" :: rest                => (mappings, rest)
+    case _                         => (mappings, Nil)
 
-  @tailrec private def parseMaps(
-    l: List[String],
-    maps: Maps = Map.empty
-  ): Maps = l match
+  @tailrec private def parseAllMappings(
+    input: List[String],
+    list: List[List[Mapping]] = Nil
+  ): List[List[Mapping]] = input match
     case s"$from-to-$to map:" :: rest =>
-      val (ranges, remainder) = parseRanges(rest)
-      parseMaps(
+      val (mappings, remainder) = parseMappings(rest)
+      parseAllMappings(
         remainder,
-        maps.updated(from, to -> ranges),
+        mappings :: list,
       )
-    case _                            => maps
+    case _                            => list.reverse
 
-  private def findRange(value: Long, ranges: Ranges): Option[Range] =
-    ranges.maxBefore(value).map(_._2).filter(_.contains(value))
+  private def transform(value: Long, mappings: List[Mapping]): Long =
+    mappings.findMapM[Id, Long](_.map(value)) | value
 
-  @tailrec private def location(value: Long, from: String, maps: Maps): Long =
-    maps.get(from) match
-      case Some((to, ranges)) =>
-        val result = findRange(value, ranges).cata(_.apply(value), value)
-        location(result, to, maps)
-      case None               => value
+  private def combine(as: List[Mapping], bs: List[Mapping]): List[Mapping] =
+    val aPoints = as.flatMap(a => a.src :: (a.dst + a.len) :: Nil)
+    val bPoints = bs.flatMap(b => b.src :: (b.src + b.len) :: Nil)
+    val aSplit  = bPoints.foldLeft(as): (mappings, point) =>
+      mappings.flatMap: range =>
+        split(range, point - range.dst)
+    val bSplit  = aPoints.foldLeft(bs): (mappings, point) =>
+      mappings.flatMap: range =>
+        split(range, point - range.src)
+    aSplit.map(a => a.copy(dst = transform(a.dst, bs))) ++
+      bSplit.filterNot(b => as.exists(_.mapsTo(b.src)))
+
+  private def split(range: Mapping, n: Long): List[Mapping] =
+    if (n > 0 && n < range.len) range.take(n) :: range.drop(n) :: Nil
+    else range :: Nil
 
   override def a(lines: Vector[String]): Long =
-    val seeds = NumRe.findAllIn(lines.head).map(_.toLong).toList
-    val maps  = parseMaps(lines.drop(2).toList)
-    seeds.map(location(_, "seed", maps)).min
+    val seeds        = NumRe.findAllIn(lines.head).map(_.toLong).toList
+    val head :: tail = parseAllMappings(lines.drop(2).toList): @unchecked
+    val mappings     = tail.foldLeft(head)(combine)
+    seeds.map(transform(_, mappings)).min
   end a
 
-  // brute force. todo: just combine all the ranges
   override def b(lines: Vector[String]): Long =
-    val pairs = NumRe.findAllIn(lines.head).map(_.toLong).toList.grouped(2)
-    val maps  = parseMaps(lines.drop(2).toList)
-    val seeds = pairs.flatMap(pair => pair.head to pair.head + pair.last)
-    seeds.map(location(_, "seed", maps)).min
+    val pairs        = NumRe.findAllIn(lines.head).map(_.toLong).toList.grouped(2)
+    val head :: tail = parseAllMappings(lines.drop(2).toList): @unchecked
+    val mappings     = tail.foldLeft(head)(combine)
+    val seeds        =
+      pairs.toList.map(pair => Mapping(pair.head, pair.head, pair.last))
+    val values       = seeds.map(seed => transform(seed.src, mappings)) ++
+      mappings.filter(mapping => seeds.exists(_.maps(mapping.src))).map(_.dst)
+    values.min
   end b
 
 end Day5
