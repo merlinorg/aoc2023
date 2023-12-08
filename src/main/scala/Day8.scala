962e26f696a6963c46d598a79e32839daa17125f
merlin <merlin@merlin.org>
2023-12-08 01:05:18 -0500

    fix

--- src/main/scala/Day8.scala
+++ src/main/scala/Day8.scala
@@ -3,24 +3,24 @@ package org.merlin.aoc2023
 import scala.annotation.tailrec
 
 object Day8 extends AoC:
-  private def parse(lines: Vector[String]): (Iterator[Char], Map[String, Map[Char, String]]) =
-    val llr = Iterator.continually(lines.head).flatten
-    val rules = lines
+  private def parse(
+    lines: Vector[String]
+  ): (String, Map[String, Map[Char, String]]) =
+    lines.head -> lines
       .drop(2)
       .map:
         case s"$x = ($l, $r)" =>
           x -> Map('L' -> l, 'R' -> r)
       .toMap
-    llr -> rules
 
   override def a(lines: Vector[String]): Long =
-    val (llr, rules) = parse(lines)
+    val (lr, rules) = parse(lines)
 
     @tailrec def loop(from: String, lr: Iterator[Char], count: Long): Long =
       val next = rules(from)(lr.next())
       if (next == "ZZZ") count else loop(next, lr, 1 + count)
 
-    loop("AAA", llr, 1)
+    loop("AAA", Iterator.continually(lr).flatten, 1)
   end a
 
   // https://stackoverflow.com/questions/40875537/fp-lcm-in-scala-in-1-line
@@ -34,13 +34,17 @@ object Day8 extends AoC:
         .abs
 
   override def b(lines: Vector[String]): Long =
-    val (llr, rules) = parse(lines)
+    val (lr, rules) = parse(lines)
 
     @tailrec def loop(from: String, lr: Iterator[Char], count: Long): Long =
       val next = rules(from)(lr.next())
       if (next.endsWith("Z")) count else loop(next, lr, 1 + count)
 
-    lcm(rules.keys.filter(_.endsWith("A")).map(loop(_, llr, 1)))
+    lcm(
+      rules.keys
+        .filter(_.endsWith("A"))
+        .map(loop(_, Iterator.continually(lr).flatten, 1))
+    )
   end b
 
 end Day8
