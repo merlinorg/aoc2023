58e770ad05c2b04564bcb250bb2f669b8fae5e2b
merlin <merlin@merlin.org>
2023-12-09 09:51:22 -0500

    maybe cleaner

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -4,38 +4,28 @@ import scala.annotation.tailrec
 
 object Day9 extends AoC:
 
-  private def getDiffs(v: Vector[Long]): Vector[Long] =
-    v.sliding(2).map(pair => pair.last - pair.head).toVector
-
-  @tailrec private def aLoop(v: Vector[Long], sum: Long): Long =
-    val diffs = getDiffs(v)
-    if (diffs.forall(_ == 0))
-      v.last + sum
-    else
-      aLoop(diffs, v.last + sum)
+  @tailrec private def loop(v: List[Long], sum: Long): Long =
+    v.zip(v.tail).map(_ - _) match
+      case Nil   =>
+        v.head + sum
+      case diffs =>
+        loop(diffs, v.head + sum)
 
   override def a(lines: Vector[String]): Long =
     lines
       .map: line =>
-        NumRe.findAllIn(line).map(_.toLong).toVector
+        NumRe.findAllIn(line).map(_.toLong).toList.reverse
       .map: values =>
-        aLoop(values, 0)
+        loop(values, 0)
       .sum
   end a
-
-  private def bLoop(v: Vector[Long]): Long =
-    val diffs = getDiffs(v)
-    if (diffs.forall(_ == 0))
-      v.head
-    else
-      v.head - bLoop(diffs)
 
   override def b(lines: Vector[String]): Long =
     lines
       .map: line =>
-        NumRe.findAllIn(line).map(_.toLong).toVector
+        NumRe.findAllIn(line).map(_.toLong).toList
       .map: values =>
-        bLoop(values)
+        loop(values, 0)
       .sum
   end b
 
