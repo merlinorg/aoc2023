578d5f3180f84b21b2e92a59425fcb38ef7445ab
merlin <merlin@merlin.org>
2023-12-13 10:36:35 -0500

    braces? what year is this

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -12,22 +12,19 @@ object Day13 extends AoC:
   private def differences(r0: Vector[Char], r1: Vector[Char]): Int =
     r0.zip(r1).count(_ != _)
 
-  private def reflections(a: Area, smudges: Int): Seq[Int] = {
-    for {
+  private def reflections(a: Area, smudges: Int): Seq[Int] =
+    for
       row            <- a.indices.drop(1)
       (before, after) = a.splitAt(row)
       if smudges == before.reverse.zip(after).foldMap(differences)
-    } yield row
-  }
+    yield row
 
   override def a(lines: Vector[String]): Long =
     parseAreas(lines).foldMap: area =>
       reflections(area, 0).map(_ * 100).sum + reflections(area.transpose, 0).sum
-  end a
 
   override def b(lines: Vector[String]): Long =
     parseAreas(lines).foldMap: area =>
       reflections(area, 1).map(_ * 100).sum + reflections(area.transpose, 1).sum
-  end b
 
 end Day13
