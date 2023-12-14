9f2dbf6908611b8c1a677cb28ea7a3ff72a1ddd0
merlin <merlin@merlin.org>
2023-12-14 08:55:58 -0500

    day 14 clean

--- src/main/scala/Day13.scala
+++ src/main/scala/Day13.scala
@@ -27,4 +27,16 @@ object Day13 extends AoC:
     parseAreas(lines).foldMap: area =>
       reflections(area, 1).map(_ * 100).sum + reflections(area.transpose, 1).sum
 
+// 1.  Source
+// 2.   .fromResource("day-13.txt")
+// 3.   .getLines
+// 4.   .toVector
+// 5.   .selectSplit(_.nonEmpty)
+// 6.   .map(_.toVector.map(_.toVector))
+// 7.   .foldMap: area =>
+// 8.     def reflections(a: Vector[Vector[Char]]): Option[Int] =
+// 9.       a.indices.drop(1).find: row =>
+// A.         a.take(row).reverse.zip(a.drop(row)).foldMap((r0, r1) => (r0 zip r1).count(_ != _)) == 1
+// B.     reflections(area).foldMap(_ * 100) + reflections(area.transpose).sum
+
 end Day13
---
+++ src/main/scala/Day14.scala
@@ -0,0 +1,46 @@
+package org.merlin.aoc2023
+
+import scalaz.*
+import scalaz.Scalaz.*
+
+import scala.collection.mutable
+
+object Day14 extends AoC:
+  private def tilt(lines: Vector[String]): Vector[String] =
+    val array = lines.toArray.map(_.toArray)
+    for
+      y  <- lines.indices.drop(1)
+      x  <- lines.indices
+      if array(y)(x) == 'O'
+      y2 <- (y to 0 by -1).find(y2 => y2 == 0 || array(y2 - 1)(x) != '.').filter(_ < y)
+    do
+      array(y2)(x) = 'O'
+      array(y)(x) = '.'
+    array.toVector.map(_.mkString)
+
+  private def load(lines: Vector[String]): Long =
+    lines.zipWithIndex.foldMap:
+      case (line, y) => line.count(_ == 'O') * (lines.length - y)
+
+  private def rotate(lines: Vector[String]): Vector[String] =
+    lines.transpose.map(_.reverse.mkString)
+
+  override def a(lines: Vector[String]): Long =
+    load(tilt(lines))
+
+  override def b(lines: Vector[String]): Long =
+    val cache  = mutable.Map.empty[Vector[String], Int]
+    val result = for
+      (from, to) <- Iterator
+                      .iterate(lines): l =>
+                        (0 until 4).foldLeft(l)((l, _) => rotate(tilt(l)))
+                      .zipWithIndex
+                      .map: (l, i) =>
+                        i -> cache.getOrElseUpdate(l, i)
+                      .find(_ > _)
+      lastIndex   = to + (1000000000 - from) % (from - to)
+      last        = cache.map(_.swap)(lastIndex)
+    yield load(last)
+    result | 0
+
+end Day14
