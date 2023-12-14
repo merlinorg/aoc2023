2e4ea817575fc8e7b5cd03d5f4ff5b496b4de3b1
merlin <merlin@merlin.org>
2023-12-14 12:59:12 -0500

    golf

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -6,52 +6,47 @@ import scalaz.Scalaz.*
 import scala.annotation.tailrec
 
 object Day14 extends AoC:
-  private def tilt(lines: Vector[String]): Vector[String] =
-    val array = lines.toArray.map(_.toArray)
-    for
-      y  <- lines.indices.tail
-      x  <- lines.indices
-      yᛌ <- (y to 0 by -1).find(yᛌ => yᛌ == 0 || array(yᛌ - 1)(x) != '.')
-      if array(y)(x) == 'O' && yᛌ < y
-    do
-      array(yᛌ)(x) = 'O'
-      array(y)(x) = '.'
-    array.toVector.map(_.mkString)
+  private type Area = Vector[String]
+  
+  private def tilt(lines: Area): Area =
+    lines.map: line =>
+      "#+|[^#]+".r.findAllIn(line).map(_.sortBy(_ != 'O')).mkString
 
-  private def load(lines: Vector[String]): Long =
-    lines.zipWithIndex.foldMap:
-      case (line, y) => line.count(_ == 'O') * (lines.length - y)
+  private def load(lines: Area): Long =
+    lines.foldMap: line =>
+      line.toList.zipWithIndex.foldMap:
+        case (c, i) => if (c == 'O') lines.length - i else 0
 
-  private def rotate(lines: Vector[String]): Vector[String] =
+  private def rotate(lines: Area): Area =
     lines.transpose.map(_.reverse.mkString)
 
-  override def a(lines: Vector[String]): Long =
-    load(tilt(lines))
+  private def rotateᛌ(lines: Area): Area =
+    lines.map(_.reverse).transpose.map(_.mkString)
 
-  override def b(lines: Vector[String]): Long =
-    @tailrec def loop(l: Vector[String], map: Map[Vector[String], Int]): Long = map.get(l) match
-      case Some(to) => load(map.map(_.swap)(to + (1000000000 - map.size) % (map.size - to)))
+  override def a(lines: Area): Long =
+    load(tilt(rotateᛌ(lines)))
+
+  override def b(lines: Area): Long =
+    @tailrec def loop(l: Area, map: Map[Area, Int]): Area = map.get(l) match
+      case Some(to) => map.map(_.swap)(to + (1000000000 - map.size) % (map.size - to))
       case None     => loop((0 until 4).foldLeft(l)((l, _) => rotate(tilt(l))), map.updated(l, map.size))
-    loop(lines, Map.empty)
+    load(loop(rotateᛌ(lines), Map.empty))
 
 end Day14
 
-// Part 2 in 15 lines
+// Part 2 in 11 lines
 
-// def tilt(l: Vector[String]): Vector[String]                                         =
-//   val array = l.toArray.map(_.toArray)
-//   while l.indices.tail
-//     .flatMap(l.indices.toList.strengthR)
-//     .filter((x, y) => array(y)(x) == 'O' && array(y - 1)(x) == '.')
-//     .map((x, y) => (array(y - 1)(x) = 'O', array(y)(x) = '.'))
-//     .nonEmpty
-//   do ()
-//   array.toVector.map(_.mkString)
+// def tilt(lines: Vector[String]): Vector[String] =
+//   lines.map: line =>
+//     "#+|[^#]+".r.findAllIn(line).map(_.sortBy(_ != 'O')).mkString
 //
 // @tailrec def loop(l: Vector[String], map: Map[Vector[String], Int]): Vector[String] = map.get(l) match
 //   case Some(to) => map.map(_.swap)(to + (1000000000 - map.size) % (map.size - to))
 //   case None     =>
 //     loop((0 until 4).foldLeft(l)((l, _) => tilt(l).transpose.map(_.reverse.mkString)), map.updated(l, map.size))
 //
-// loop(Source.fromResource("day-14.txt").getLines.toVector, Map.empty).zipWithIndex.foldMap: (line, y) =>
-//   line.count(_ == 'O') * (line.length - y)
+// val lines = Source.fromResource("day-14.txt").getLines.toVector.map(_.reverse).transpose.map(_.mkString)
+//
+// loop(lines, Map.empty).foldMap: line =>
+//   line.toList.zipWithIndex.foldMap:
+//     case (c, i) => if (c == 'O') line.length - i else 0
