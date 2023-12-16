1b267dfc7df67aaaa0c5a79742be31327d2f509a
merlin <merlin@merlin.org>
2023-12-16 10:09:38 -0500

    more pure

--- src/main/scala/Day16.scala
+++ src/main/scala/Day16.scala
@@ -3,44 +3,48 @@ package org.merlin.aoc2023
 import scalaz.*
 import scalaz.Scalaz.*
 
-import scala.collection.mutable
-
 object Day16 extends AoC:
-  private case class Beam(x: Int, y: Int, dx: Int, dy: Int):
-    def step: Beam = copy(x = x + dx, y = y + dy)
+  private final case class Photon(x: Int, y: Int, dx: Int, dy: Int): // A certain photon
+    def location: (Int, Int) = (x, y)
 
-  private def energized(lines: Vector[String], beam0: Beam): Long =
-    val visited = mutable.Set.empty[(Int, Int)]
-    val dedup   = mutable.Set.empty[Beam]
-    val beams   = mutable.Queue(beam0)
-    while (beams.nonEmpty)
-      val beam = beams.dequeue()
-      if (dedup.add(beam))
-        val next = beam.step
-        if (next.x >= 0 && next.x < lines.length && next.y >= 0 && next.y < lines.length)
-          visited.add(next.x -> next.y)
-          lines(next.y)(next.x) match
-            case '/'                 =>
-              beams.enqueue(next.copy(dx = -next.dy, dy = -next.dx))
-            case '\\'                =>
-              beams.enqueue(next.copy(dx = next.dy, dy = next.dx))
-            case '|' if next.dx != 0 =>
-              beams.enqueue(next.copy(dx = 0, dy = -1), next.copy(dx = 0, dy = 1))
-            case '-' if next.dy != 0 =>
-              beams.enqueue(next.copy(dx = -1, dy = 0), next.copy(dx = 1, dy = 0))
-            case _                   =>
-              beams.enqueue(next)
-    visited.size
+    def move: Photon = copy(x = x + dx, y = y + dy)
 
-  override def part1(lines: Vector[String]): Long =
-    energized(lines, Beam(-1, 0, 1, 0))
+    def redirect(dx: Int, dy: Int): Photon = copy(dx = dx, dy = dy)
 
-  override def part2(lines: Vector[String]): Long =
-    lines.indices
-      .flatMap: z =>
-        List(Beam(-1, z, 1, 0), Beam(lines.length, z, -1, 0), Beam(z, -1, 0, 1), Beam(z, lines.length, 0, -1))
-      .map: beam =>
-        energized(lines, beam)
+    def reflect(chr: Char): List[Photon] = chr match
+      case '/'            => List(redirect(-dy, -dx))
+      case '\\'           => List(redirect(dy, dx))
+      case '|' if dx != 0 => List(redirect(0, -1), redirect(0, 1))
+      case '-' if dy != 0 => List(redirect(-1, 0), redirect(1, 0))
+      case _              => List(this)
+
+  private final case class State(energized: Set[(Int, Int)], seen: Set[Photon], photons: List[Photon]):
+    def update(board: Vector[String]): State =
+      val next = nextPhotons(board)
+      State(energized ++ next.map(_.location), seen ++ photons, next)
+
+    private def nextPhotons(board: Vector[String]): List[Photon] =
+      for
+        photon    <- photons if !seen.contains(photon)
+        moved      = photon.move if moved.x >= 0 && moved.x < board.length && moved.y >= 0 && moved.y < board.length
+        reflected <- moved.reflect(board(moved.y)(moved.x))
+      yield reflected
+
+  private def energized(photon0: Photon, board: Vector[String]): Long =
+    Iterator
+      .iterate(State(Set.empty, Set.empty, List(photon0)))(_.update(board))
+      .find(_.photons.isEmpty)
+      .cata(_.energized.size, 0)
+
+  override def part1(board: Vector[String]): Long =
+    energized(Photon(-1, 0, 1, 0), board)
+
+  override def part2(board: Vector[String]): Long =
+    board.indices
+      .flatMap: i =>
+        List(Photon(-1, i, 1, 0), Photon(board.length, i, -1, 0), Photon(i, -1, 0, 1), Photon(i, board.length, 0, -1))
+      .map: photon =>
+        energized(photon, board)
       .max
 
 end Day16
