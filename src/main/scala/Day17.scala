6a35529be579dc6ad076bad5487d620725c49dda
merlin <merlin@merlin.org>
2023-12-17 08:52:53 -0500

    more efficient

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -15,24 +15,22 @@ object Day17 extends AoC:
 
     def turnᛌ: Crucible = copy(x = x - dy, y = y - dx, dx = -dy, dy = -dx, count = 1)
 
-    def within(city: Vector[String]): Boolean = x >= 0 && y >= 0 && x < city.length && y < city.length
+    def within(city: Vector[String]): Boolean = x >= 0 && y >= 0 && x < city.head.length && y < city.length
 
-    def loseHeat(city: Vector[String]): Crucible = if (within(city)) copy(loss = loss + city(y)(x) - '0') else this
+    def loseHeat(city: Vector[String]): Crucible = copy(loss = loss + within(city) ?? city(y)(x) - '0')
+
+  private given Ordering[Crucible] = Ordering.by(x => -x.loss)
 
   private def solve(city: Vector[String], min: Int, max: Int): Long =
-    var best    = Option.empty[Crucible]                            // ლ(ಠ益ಠლ)
-    val visited = mutable.Map.empty[(Int, Int, Int, Int, Int), Int] // key -> loss
-    val queue   = mutable.Queue(Crucible(0, 0, 1, 0, 0, 0), Crucible(0, 0, 0, 1, 0, 0))
-    while (queue.nonEmpty)
+    val seen  = mutable.Map.empty[(Int, Int, Int, Int, Int), Int] // ლ(ಠ益ಠლ) key -> loss
+    val queue = mutable.PriorityQueue(Crucible(0, 0, 1, 0, 0, 0), Crucible(0, 0, 0, 1, 0, 0))
+    while (queue.head.x != city.head.length - 1 || queue.head.y != city.length - 1 || queue.head.count < min)
       val crucible = queue.dequeue()
-      if (crucible.within(city) && visited.get(crucible.key).forall(_ > crucible.loss))
-        visited.update(crucible.key, crucible.loss)
-        if ((crucible.x == city.length - 1) && (crucible.y == city.length - 1) && (crucible.count >= min))
-          if (best.forall(_.loss > crucible.loss)) best = Some(crucible)
-        else
-          if (crucible.count < max) queue.enqueue(crucible.forward.loseHeat(city))
-          if (crucible.count >= min) queue.enqueue(crucible.turn.loseHeat(city), crucible.turnᛌ.loseHeat(city))
-    best.cata(_.loss, 0)
+      if (seen.get(crucible.key).forall(_ > crucible.loss) && crucible.within(city))
+        seen.update(crucible.key, crucible.loss)
+        if (crucible.count < max) queue.enqueue(crucible.forward.loseHeat(city))
+        if (crucible.count >= min) queue.enqueue(crucible.turn.loseHeat(city), crucible.turnᛌ.loseHeat(city))
+    queue.head.loss
 
   override def part1(city: Vector[String]): Long = solve(city, 0, 3)
 
