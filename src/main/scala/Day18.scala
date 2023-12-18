b06dc437fd89390fcc1447b8f63cb79285db4ce3
merlin <merlin@merlin.org>
2023-12-18 10:10:31 -0500

    commentary

--- src/main/scala/Day18.scala
+++ src/main/scala/Day18.scala
@@ -4,30 +4,30 @@ import scalaz.*
 import scalaz.Scalaz.*
 
 object Day18 extends AoC:
-  final val dirs  = Map("R" -> Dir.E, "D" -> Dir.S, "L" -> Dir.W, "U" -> Dir.N)
-  final val dirs2 = Map("0" -> Dir.E, "1" -> Dir.S, "2" -> Dir.W, "3" -> Dir.N)
+  private val directions1 = Map("R" -> Dir.E, "D" -> Dir.S, "L" -> Dir.W, "U" -> Dir.N)
+  private val directions2 = Map("0" -> Dir.E, "1" -> Dir.S, "2" -> Dir.W, "3" -> Dir.N)
 
-  private def parse1(lines: Vector[String]): Vector[(Dir, Int)] =
+  private def parse1(lines: Vector[String]): Vector[Vec] =
     lines.map:
       case s"$dir $count (#$_)" =>
-        dirs(dir) -> count.toInt
+        directions1(dir) * count.toInt
 
-  private def parse2(lines: Vector[String]): Vector[(Dir, Int)] =
+  private def parse2(lines: Vector[String]): Vector[Vec] =
     lines.map:
       case s"$_ $_ (#$color)" =>
-        dirs2(color.takeRight(1)) -> Integer.parseInt(color.take(5), 16)
+        directions2(color.takeRight(1)) * Integer.parseInt(color.take(5), 16)
 
-  private def area(coords: Vector[Loc]): Long =
-    coords.zip(coords.tail).map((a, b) => a.x.toLong * b.y - b.x.toLong * a.y).sum.abs / 2
+  private def area(locations: Vector[Loc]): Long =
+    locations.zip(locations.tail).map((a, b) => a.x.toLong * b.y - b.x.toLong * a.y).sum.abs / 2
 
-  private def perimeter(rules: Vector[(Dir, Int)]): Vector[Loc] =
-    rules
-      .zip(rules.tail :+ rules.head)
-      .scanLeft(false -> Loc(0, 0)): // not generally correct since I assume prior winding but shrug
-        case ((occw, loc), ((dir, count), (ndir, _))) =>
-          val ccw    = dir == ndir.ccw
-          val length = if (ccw != occw) count else if (ccw) count + 1 else count - 1
-          ccw -> (loc + dir * length)
+  private def perimeter(path: Vector[Vec]): Vector[Loc] =
+    path
+      .zip(path.tail :+ path.head)
+      .scanLeft(false -> Loc(0, 0)): // safe initial state for a 0 0 start going east
+        case ((priorWinding, location), (step, nextStep)) =>
+          val winding = nextStep.direction == step.direction.cw // coming up to a right turn
+          val edge    = if (winding != priorWinding) step else if (winding) step + 1 else step - 1
+          winding -> (location + edge)
       .map(_._2)
 
   override def part1(lines: Vector[String]): Long = lines |> parse1 |> perimeter |> area
--- src/main/scala/board.scala
+++ src/main/scala/board.scala
@@ -5,27 +5,33 @@ import scala.language.implicitConversions
 type Board = Vector[String]
 
 extension (self: Board)
-  def nw: Loc               = Loc(0, 0)
-  def se: Loc               = Loc(self.head.length - 1, self.length - 1)
-  def apply(loc: Loc): Char = self(loc.y)(loc.x)
+  def nw: Loc                     = Loc(0, 0)
+  def se: Loc                     = Loc(self.head.length - 1, self.length - 1)
+  def apply(loc: Loc): Char       = self(loc.y)(loc.x)
+  def get(loc: Loc): Option[Char] = Option.when(loc >=< self)(self(loc))
 
 enum Dir(val dx: Int, val dy: Int):
-  def cw: Dir                   = Dir.fromOrdinal((ordinal + 1) % 4)
-  def ccw: Dir                  = Dir.fromOrdinal((ordinal + Dir.values.length - 1) % 4)
-  def reverse: Dir              = Dir.fromOrdinal((ordinal + 2) % 4)
-  inline def *(scale: Int): Vec = Vec(dx * scale, dy * scale)
+  def cw: Dir                    = Dir.fromOrdinal((ordinal + 1) % 4)
+  def ccw: Dir                   = Dir.fromOrdinal((ordinal + Dir.values.length - 1) % 4)
+  def reverse: Dir               = Dir.fromOrdinal((ordinal + 2) % 4)
+  inline def *(length: Int): Vec = Vec(this, length)
 
   case N extends Dir(0, -1)
   case E extends Dir(1, 0)
   case S extends Dir(0, 1)
   case W extends Dir(-1, 0)
 
-implicit def toVec(dir: Dir): Vec = Vec(dir.dx, dir.dy)
+implicit def toVec(dir: Dir): Vec = dir * 1
 
 given Ordering[Dir] = Ordering.by(_.ordinal)
 
-final case class Vec(dx: Int, dy: Int):
-  inline def *(scale: Int): Vec = Vec(dx * scale, dy * scale)
+final case class Vec(direction: Dir, length: Int):
+  def dx: Int = direction.dx * length
+  def dy: Int = direction.dy * length
+
+  inline def +(delta: Int): Vec = copy(length = length + delta)
+  inline def -(delta: Int): Vec = copy(length = length - delta)
+  inline def *(scale: Int): Vec = copy(length = length * scale)
 
 final case class Loc(x: Int, y: Int):
   inline def +(vec: Vec): Loc           = Loc(x + vec.dx, y + vec.dy)
