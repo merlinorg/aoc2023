78fccec568a3bb3c019285dec084cb2674c64e50
merlin <merlin@merlin.org>
2023-12-20 16:46:27 -0500

    words

--- src/main/scala/Day20.scala
+++ src/main/scala/Day20.scala
@@ -7,19 +7,27 @@ object Day20 extends AoC:
 
   type ModuleName = String
 
+  // Pulses are the messages of our primary state machine. They are either low
+  // (false) or high (true) and travel from a source to a destination module
+
   final case class Pulse(
     source: ModuleName,
     destination: ModuleName,
-    value: Boolean,
+    level: Boolean,
   )
 
   object Pulse:
     final val ButtonPress = Pulse("button", "broadcaster", false)
 
+  // The modules include pass-throughs which simply forward pulses, flip flips
+  // which toggle state and emit when they receive a low pulse, and conjunctions
+  // which emit a low signal when all inputs are high.
+
   sealed trait Module:
     def name: ModuleName
     def destinations: Vector[ModuleName]
-    def pulses(value: Boolean): Vector[Pulse] = destinations.map(Pulse(name, _, value))
+    // Generate pulses for all the destinations of this module
+    def pulses(level: Boolean): Vector[Pulse] = destinations.map(Pulse(name, _, level))
 
   final case class PassThrough(
     name: ModuleName,
@@ -35,17 +43,21 @@ object Day20 extends AoC:
   final case class Conjunction(
     name: ModuleName,
     destinations: Vector[ModuleName],
+    // The source modules that most-recently sent a high pulse
     state: Set[ModuleName],
   ) extends Module
 
-  extension [A, B](self: Map[A, Vector[B]])
-    inline def add(a: A, b: B): Map[A, Vector[B]] = self.updatedWith(a):
-      case None         => Some(Vector(b))
-      case Some(values) => Some(values :+ b)
+  extension [A, B](self: Map[A, Set[B]])
+    inline def add(a: A, b: B): Map[A, Set[B]] = self.updatedWith(a):
+      case None         => Some(Set(b))
+      case Some(values) => Some(values + b)
+
+  // The machine comprises a collection of named modules and a map that gathers
+  // which modules serve as sources for each module in the machine.
 
   final case class Machine(
     modules: Map[ModuleName, Module],
-    sources: Map[ModuleName, Vector[ModuleName]]
+    sources: Map[ModuleName, Set[ModuleName]]
   ):
     inline def +(module: Module): Machine =
       copy(
@@ -56,6 +68,9 @@ object Day20 extends AoC:
   object Machine:
     final val Initial = Machine(Map.empty, Map.empty)
 
+    // To parse the input we first parse all of the modules and then fold them
+    // into a new machine
+
     def parse(lines: Vector[String]): Machine =
       val modules = lines.map:
         case s"%$name -> $targets" => FlipFlop(name, targets.split(", ").toVector, false)
@@ -63,89 +78,124 @@ object Day20 extends AoC:
         case s"$name -> $targets"  => PassThrough(name, targets.split(", ").toVector)
       modules.foldLeft(Initial)(_ + _)
 
-  final case class MachineState(
+  // The primary state machine state comprises the machine itself, the number of
+  // button presses and a queue of outstanding pulses.
+
+  final case class MachineFSM(
     machine: Machine,
     presses: Long = 0,
     queue: Queue[Pulse] = Queue.empty,
   ):
-    def nextState: MachineState = queue.dequeueOption match
+    def nextState: MachineFSM = queue.dequeueOption match
+      // If the queue is empty, we increment the button presses and enqueue a
+      // button press pulse
       case None =>
         copy(presses = presses + 1, queue = Queue(Pulse.ButtonPress))
 
-      case Some((Pulse(source, destination, value), tail)) =>
+      case Some((Pulse(source, destination, level), tail)) =>
         machine.modules.get(destination) match
+          // If a pulse reaches a pass-through, enqueue pulses for all the module
+          // destinations
           case Some(passThrough: PassThrough) =>
-            copy(queue = tail ++ passThrough.pulses(value))
+            copy(queue = tail ++ passThrough.pulses(level))
 
-          case Some(flipFlop: FlipFlop) if !value =>
+          // If a low pulse reaches a flip-flop, update the flip-flop state in the
+          // machine and enqueue pulses for all the module destinations
+          case Some(flipFlop: FlipFlop) if !level =>
             val flipFlop2 = flipFlop.copy(state = !flipFlop.state)
-            copy(machine = machine + flipFlop2, queue = tail ++ flipFlop.pulses(flipFlop2.state))
+            copy(machine = machine + flipFlop2, queue = tail ++ flipFlop2.pulses(flipFlop2.state))
 
+          // If a pulse reaches a conjunction, update the source state in the
+          // conjunction and enqueue pulses for all the module destinations
+          // according to the conjunction state
           case Some(conjunction: Conjunction) =>
             val conjunction2 =
-              conjunction.copy(state = if value then conjunction.state + source else conjunction.state - source)
-            val active       = machine.sources(conjunction.name).forall(conjunction2.state.contains)
-            copy(machine = machine + conjunction2, queue = tail ++ conjunction.pulses(!active))
+              conjunction.copy(state = if level then conjunction.state + source else conjunction.state - source)
+            val active       = machine.sources(conjunction2.name) == conjunction2.state
+            copy(machine = machine + conjunction2, queue = tail ++ conjunction2.pulses(!active))
 
+          // In all other cases just discard the pulse and proceed
           case _ =>
             copy(queue = tail)
 
-  final case class Problem1State(
+  // The problem 1 state machine comprises the number of low and high pulses
+  // processed, and whether the problem is complete (after 1000 presses). This
+  // state machine gets updated by each state of the primary state machine.
+
+  final case class Problem1FSM(
     lows: Long,
     highs: Long,
     complete: Boolean,
   ):
-    inline def +(state: MachineState): Problem1State = state.queue.headOption.map(_.value) match
-      case Some(false)                   => copy(lows = lows + 1)
-      case Some(true)                    => copy(highs = highs + 1)
+    // If the head of the pulse queue is a low or high pulse then update the
+    // low/high count. If the pulse queue is empty and the button has been pressed
+    // 1000 times then complete.
+    inline def +(state: MachineFSM): Problem1FSM = state.queue.headOption match
+      case Some(Pulse(_, _, false))      => copy(lows = lows + 1)
+      case Some(Pulse(_, _, true))       => copy(highs = highs + 1)
       case None if state.presses == 1000 => copy(complete = true)
       case None                          => this
 
+    // The result is the product of lows and highs
     def result: Long = lows * highs
 
-  object Problem1State:
-    final val Initial = Problem1State(0, 0, false)
+  object Problem1FSM:
+    final val Initial = Problem1FSM(0, 0, false)
+
+  // Part 1 is solved by first constructing the primary state machine that
+  // executes the pulse machinery. Each state of this machine is then fed to a
+  // second problem 1 state machine. We then run the combined state machines to
+  // completion.
 
   override def part1(lines: Vector[String]): Long =
     val machine = Machine.parse(lines)
     Iterator
-      .iterate(MachineState(machine))(_.nextState)
-      .scanLeft(Problem1State.Initial)(_ + _)
+      .iterate(MachineFSM(machine))(_.nextState)
+      .scanLeft(Problem1FSM.Initial)(_ + _)
       .find(_.complete)
       .get
       .result
 
-  final case class Problem2State(
+  // The problem 2 state machine is looking for the least common multiple of the
+  // cycle lengths of the subgraphs that feed into the output "rx" module. When it
+  // observes a high pulse from the final module of one these subgraphs, it
+  // records the number of button presses to reach this state.
+
+  final case class Problem2FSM(
     cycles: Map[ModuleName, Long],
   ):
-    inline def +(state: MachineState): Problem2State = state.queue.headOption match
-      case Some(Pulse(_, dst, false)) if cycles.get(dst).contains(0L) =>
-        copy(cycles = cycles + (dst -> state.presses))
-      case _                                                          => this
+    inline def +(state: MachineFSM): Problem2FSM = state.queue.headOption match
+      case Some(Pulse(src, _, true)) if cycles.contains(src) => copy(cycles = cycles + (src -> state.presses))
+      case _                                                 => this
 
+    // We are complete if we have the cycle value for each subgraph
     def complete: Boolean = cycles.values.forall(_ > 0)
     def result: Long      = lcm(cycles.values)
 
     private def lcm(list: Iterable[Long]): Long      = list.foldLeft(1L)((a, b) => b * a / gcd(a, b))
     @tailrec private def gcd(x: Long, y: Long): Long = if y == 0 then x else gcd(y, x % y)
 
-  object Problem2State:
-    def apply(names: Vector[ModuleName]): Problem2State =
-      new Problem2State(names.map(_ -> 0L).toMap)
+  object Problem2FSM:
+    def apply(machine: Machine): Problem2FSM =
+      new Problem2FSM(subgraphs(machine).map(_ -> 0L).toMap)
 
-  def subgraphs(machine: Machine): Vector[ModuleName] =
-    val terminal = machine.sources.keys.find(!machine.modules.contains(_)).get
-    (machine.sources(terminal): @unchecked) match
-      case Vector(source) =>
-        (machine.modules(source): @unchecked) match
-          case Conjunction(name, _, _) =>
-            machine.sources(name)
+    // The problem is characterized by a terminal module ("rx") that is fed by
+    // several subgraphs so we look to see which are the sources of the terminal
+    // module; these are the subgraphs whose cycle lengths we need to count.
+    private def subgraphs(machine: Machine): Set[ModuleName] =
+      val terminal = (machine.sources.keySet -- machine.modules.keySet).head
+      machine.sources(machine.sources(terminal).head)
+
+  // Part 2 is solved by first constructing the primary state machine that
+  // executes the pulse machinery. Each state of this machine is then fed to a
+  // second problem 2 state machine. We then run the combined state machines to
+  // completion.
 
   override def part2(lines: Vector[String]): Long =
     val machine = Machine.parse(lines)
     Iterator
-      .iterate(MachineState(machine))(_.nextState)
-      .scanLeft(Problem2State(subgraphs(machine)))(_ + _)
+      .iterate(MachineFSM(machine))(_.nextState)
+      .scanLeft(Problem2FSM(machine))(_ + _)
       .find(_.complete)
       .get
       .result
